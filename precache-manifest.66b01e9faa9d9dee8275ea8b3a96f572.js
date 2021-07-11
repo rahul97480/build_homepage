595c813da98f8bcbe290384a63806f8d193c2e42
Rahul Gupta <guptarahul97480@gmail.com>
2021-07-11 22:02:19 +0530

    Updating

---
+++ precache-manifest.66b01e9faa9d9dee8275ea8b3a96f572.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3158f6d0f5133df947939820545ef479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5185297efb326f1afa30",
+    "url": "/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "ad6b38e719b303b0349c",
+    "url": "/static/css/main.16516185.chunk.css"
+  },
+  {
+    "revision": "5185297efb326f1afa30",
+    "url": "/static/js/2.d399e5a7.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/static/js/2.d399e5a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad6b38e719b303b0349c",
+    "url": "/static/js/main.89107e70.chunk.js"
+  },
+  {
+    "revision": "60949e83de0aeb529ab1",
+    "url": "/static/js/runtime-main.25c7e1d9.js"
+  },
+  {
+    "revision": "314ce54b0557f9ea8e6e13fb28bef08b",
+    "url": "/static/media/API.314ce54b.jpg"
+  },
+  {
+    "revision": "f33ffc54f9c68454fedc729025a0a97a",
+    "url": "/static/media/Covid.f33ffc54.jpg"
+  },
+  {
+    "revision": "e1c278e0d0fccab1a602b34b210acb6d",
+    "url": "/static/media/Ecom.e1c278e0.jpg"
+  },
+  {
+    "revision": "74c1068874a2f5e2c00ed3d5a9993b87",
+    "url": "/static/media/JS.74c10688.jpg"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/static/media/software.43049cae.jpeg"
+  }
+]);
